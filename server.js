9776d291dde806aef104e9805f082036d63781ce
Sesha Machiraju <sesha.machiraju@avanade.com>
2019-09-29 16:27:01 -0700

    initial

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+"use strict";
+
 const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
 
-app.listen(process.env.PORT || 8080); 
+if (require.main === module) {
+    app.listen(process.env.PORT || 8080, function() {
+    });
+}
 
 module.exports = app;
