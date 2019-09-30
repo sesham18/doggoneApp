cf69980a87638ac5602f041c109a360e69b337ad
Sesha Machiraju <sesha.machiraju@avanade.com>
2019-09-29 18:33:54 -0700

    initial

--- test/test.js
+++ test/test.js
@@ -6,14 +6,3 @@ const app  = require("../server");
 const expect = chai.expect;
 
 chai.use(chaiHttp);
-
-describe("index page", function() {
-    it ("exists", function() {
-        return(chai)
-            .request(app)
-            .get("/")
-            .then(function(res){
-                expect(res).to.have.status(200);
-            });
-    });
-})
